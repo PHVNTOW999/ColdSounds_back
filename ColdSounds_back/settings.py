2c2dc874c7f3203cc136ce10cb379b60196b077a
Ali Tim <ali.tim03.at@gmail.com>
2023-03-10 06:18:36 +0600

    update

--- ColdSounds_back/settings.py
+++ ColdSounds_back/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'main',
+    'metadatas',
     'corsheaders'
 ]
 
--- ColdSounds_back/urls.py
+++ ColdSounds_back/urls.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from django.urls import path
 
 from main.views import SingersList, SinglsList, AlbumsList, EPList
+from metadatas.views import MenuList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/singers/', SingersList.as_view(), name='singers_list'),
-    path('api/singls/', SinglsList.as_view(), name='singls_list'),
-    path('api/albums/', AlbumsList.as_view(), name='albums_list'),
-    path('api/ep/', EPList.as_view(), name='ep_list')
+
+    # main
+    path(r'api/singers/', SingersList.as_view(), name='singers_list'),
+    path(r'api/singls/', SinglsList.as_view(), name='singles_list'),
+    path(r'api/albums/', AlbumsList.as_view(), name='albums_list'),
+    path(r'api/ep/', EPList.as_view(), name='ep_list'),
+
+    # metadatas
+    path('api/menu/', MenuList.as_view(), name='menu_list')
 ]
